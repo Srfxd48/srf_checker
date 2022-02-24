abaa34b44d787cab3ac3fbd80062d62720d30dfe
Srfxd48 <99257068+Srfxd48@users.noreply.github.com>
2022-02-24 17:41:11 +0530

    Update ss.php

--- modules/checker/ss.php
+++ modules/checker/ss.php
@@ -62,7 +62,7 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
         
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, 'https://m.stripe.com/6');
-            curl_setopt($ch, CURLOPT_PROXY, $url[array_rand($url)]);
+            curl_setopt($ch, CURLOPT_PROXY, $url[array_null($url)]);
             curl_setopt($ch, CURLOPT_PROXYUSERPWD, $userpass[array_rand($userpass)]);
             curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
             curl_setopt($ch, CURLOPT_HEADER, 0);
